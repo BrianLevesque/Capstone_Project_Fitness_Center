5bbe88f5aba48db8715c08ac909a3442ae2a1274
Brian Levesque <kaka10@comcast.net>
2024-05-10 15:30:30 -0400

    main body mostly done

--- main.py
+++ main.py
@@ -16,18 +16,134 @@ club4 = members_clubs.Club('Oceanside Members Club','3540 Ocean Blvd')
 
 clubs = [club1, club2, club3, club4]
 
-# def main():
-print('Welcome to GC Fitness Club! ')
-
-while True:
-    ans = input('Are you currently a member? (y/n) ')
-    if ans == 'y':
-        name = input('Please enter your name to get started: ')
-    elif ans == 'n':
-        break
-    else:
-        print('try again')
+member_list = []
 
 
-"""if __init__ == '__main__':
-    main()"""
+
+#signs up member for single club membership
+def sign_up_single():
+    name = input('Please enter your name to get started: ')
+    member_id = len(members_clubs.Club.Members) + 1
+    j = 1
+    for i in clubs:
+        print(f"{j}. {i.name}")
+        j += 1
+    while True:
+        try:
+            club = int(input("which could would you like to join? (please enter the number)")) - 1
+            if club < len(clubs):
+                new_mem = members_clubs.Single_club_member(member_id, name, clubs[club])
+                members_clubs.Club.Members.append(new_mem)
+                break
+            else:
+                print(f"Please enter a valid number 1-{len(clubs)}")
+        except ValueError:
+                print(f"Please enter value number 1-{len(clubs)}")
+    return name
+
+
+#signs up a member to a multi-club membership
+def sign_up_multiple():
+    name = input('Please enter your name to get started: ')
+    member_id = len(members_clubs.Club.Members) + 1
+    new_mem = members_clubs.Multi_club_member(member_id, name, 0)
+    members_clubs.Club.Members.append(new_mem)
+    return name
+
+#checks membership of person
+def check_membership(name):
+    for i in members_clubs.Club.Members:
+        if name == i.name:
+            return True
+    return False
+
+def main():
+    print('Welcome to GC Fitness Club! ')
+
+    while True:
+        ans = input('Are you currently a member? (y/n) ')
+
+        if ans == 'y':
+            name = input("Please enter your name: ")
+            if check_membership(name):
+                break
+            else:
+                print("We do not see your membership")
+                continue
+        elif ans == 'n':
+            while True:
+                sign_up = input("Would you like to sign up?(y/n)")
+                if sign_up == "y":
+                    member_type = input("Would you like to be a single club member or multi-club member?(s/m)")
+                    if member_type == "s":
+                        name = sign_up_single()
+                        break
+                    elif member_type == "m":
+                        name = sign_up_multiple()
+                        break
+                    else:
+                        print("please enter valid input")
+            break
+
+        else:
+            print('Please enter valid input')
+#start of asking task
+    while True:
+
+        print("""1. Check in
+2. Pay fee
+3. Display member information
+4. Cancel membership
+5. Add member""")
+        try:
+            like_to_do = int(input("What would you like to do?(Please enter number) "))
+            if like_to_do > 5 or like_to_do < 1:
+                print("please enter valid number")
+
+        except ValueError:
+            print('Please enter valid number')
+
+        for i in members_clubs.Club.Members:
+            if name == i.name:
+                mem = i
+
+        if like_to_do == 1:
+            j = 1
+            for i in clubs:
+                print(f"{j}. {i.name}")
+                j += 1
+            club_check_in = int(input("what club would you like to check into?(please enter a number) ")) - 1
+            mem.check_in(clubs[club_check_in])
+        elif like_to_do == 2:
+            mem.pay_fee()
+            print(mem)
+
+        elif like_to_do == 3:
+            print(mem)
+
+        elif like_to_do == 4:
+            members_clubs.Club.remove_member(club1,mem)
+
+        elif like_to_do == 5:
+            name = input("What is the name of the member to be added?")
+            member_type = input("Would you like to be a single club member or multi-club member?(s/m)")
+            if member_type == "s":
+                name = sign_up_single()
+            elif member_type == "m":
+                name = sign_up_multiple()
+            else:
+                print("please enter valid input")
+        c_task = input("Do you want to complete another task?(y/n)")
+        if c_task == "y":
+            continue
+        else:
+            print("Thank you for visting")
+            break
+
+
+
+
+
+
+#if __init__ == '__name__':
+main()
--- members_clubs.py
+++ members_clubs.py
@@ -5,27 +5,83 @@ class Member:
     def __init__(self, member_id, name):
         self.member_id = member_id
         self.name = name
+        self.fee_paid = False
     def check_in(self, club):
         return None
 
+    def pay_fee(self):
+        self.fee_paid = True
+
+    def __str__(self):
+        return f"Member name is {self.name} and the member id is {self.member_id}"
 class Single_club_member(Member):
 
     def __init__(self, member_id, name, club):
-        Member.__init__(member_id, name)
+        Member.__init__(self,member_id, name)
         self.club = club
 
     def check_in(self, club):
-        if club != self.club:
-            return "I'm sorry this is the wrong club"
+        if club == self.club:
+            if self.fee_paid:
+                print("You have been checked in!")
+            else:
+                print("Please pay your fee before checking in.")
+
+        else:
+            print("I'm sorry this is the wrong club")
+
+    def pay_fee(self):
+        while True:
+
+            fee_confirm = input("Your fee is $10 for a single club membership please confirm (y/n)")
+            if fee_confirm == "y":
+                self.fee_paid = True
+                print("your payment has been confirmed")
+                break
+            elif fee_confirm == "n":
+                print("your payment has been canceled")
+                break
+            else:
+                print("please enter a valid response")
+
+
+
+
+    def __str__(self):
+        if self.fee_paid:
+            return f"Member name is {self.name} and the member id is {self.member_id}. Club is {self.club.name}. Your fee has been paid"
+        else:
+            return f"Member name is {self.name} and the member id is {self.member_id}. Club is {self.club.name}. Your fee has not been paid"
 
 class Multi_club_member(Member):
 
     def __init__(self, member_id, name, points):
-        Member.__init__(member_id, name)
+        Member.__init__(self,member_id, name)
         self.points = points
     def check_in(self, club):
-        self.points += 1
+        if self.fee_paid:
+            print("You have been checked in!")
+            self.points += 1
+        else:
+            print("Please pay your fee before checking in.")
+    def pay_fee(self):
+        while True:
 
+            fee_confirm = input("Your fee is $20 for a multi-club membership please confirm (y/n)")
+            if fee_confirm == "y":
+                self.fee_paid = True
+                print("your payment has been confirmed")
+                break
+            elif fee_confirm == "n":
+                print("your payment has been canceled")
+                break
+            else:
+                print("please enter a valid response")
+    def __str__(self):
+        if self.fee_paid:
+            return f"Member name is {self.name} and the member id is {self.member_id}. Membership points are {self.points} and your fee has been paid"
+        else:
+            return f"Member name is {self.name} and the member id is {self.member_id}. Membership points are {self.points} and your fee has not been paid"
 # Create Club Objects
 class Club():
     Members = []
@@ -37,3 +93,11 @@ class Club():
     # adds members
     def add_members(self, member):
         Club.Members.append(member)
+
+
+
+
+    # removes member from members list
+    def remove_member(self,member):
+        Club.Members.remove(member)
+        print(f"{member.name} has been successfully unsubscribed.")
